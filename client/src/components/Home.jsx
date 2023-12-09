f5ea7c27c3ff1a8a5bf601afe572a379160fd487
Yash <yashkamble772@gmail.com>
2023-12-09 20:03:46 +0530

    'abc'

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { Header } from "./Header";
 export function Home(){
     return (
         <Container>
+       <p>Hey</p>
         <Header text="Welcome to the Trek planner app"></Header>
         <p>Using this app you can see the new adventurous trek plans and can register.</p>
         </Container>
